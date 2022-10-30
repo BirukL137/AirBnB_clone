289a770606bc71f793d17fd5e6f00fbfd5f3945c
BirukL137 <buraf13k@gmail.com>
2022-10-30 06:39:42 -0700

    models

--- models/base_model.py
+++ models/base_model.py
@@ -2,30 +2,43 @@
 """Contains class defines all common attributes/methods for other classes"""
 from uuid import uuid4
 from datetime import datetime
+import models
+
 
 class BaseModel:
     """defines common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialize attributes of BaseModel class
         """
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        iso_format = "%Y-%m-%dT%H:%M:%S.%f"
 
-    def __str__(self):
-        """modified string representation of object"""
-        return f'[{type(self).__name__}] ({self.id}) {self.__dict__}'
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    self.__dict__[key] = datetime.strptime(value, iso_format)
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         """updates public instance attr 'updated_at' with current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of the __dict__
         instance
         """
-        my_dict = self.__dict__.copy()
-        my_dict['__class__'] = type(self).__name__
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
-        return my_dict
+        custom_dict = self.__dict__.copy()
+        custom_dict["created_at"] = self.created_at.isoformat()
+        custom_dict["updated_at"] = self.updated_at.isoformat()
+        custom_dict["__class__"] = self.__class__.__name__
+        return custom_dict
 
+    def __str__(self):
+        """modified string representation of object"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
